58b9a3562106bc6ea4e75b1b99e2a615e8877eff
Chiro Liang <Chiro2001@163.com>
2022-09-16 15:45:56 +0800

    LexicalAnalyzer: parsing DFA

--- src/cn/edu/hitsz/compiler/error/ErrorDescription.java
+++ src/cn/edu/hitsz/compiler/error/ErrorDescription.java
@@ -8,4 +8,5 @@ package cn.edu.hitsz.compiler.error;
 public class ErrorDescription {
     public final static String NO_SYMBOL = "No such symbol: %s";
     public final static String HAS_SYMBOL = "Duplicated symbol: %s";
+    public final static String LEX_NO_STATE = "No such DFA State: %d";
 }
--- src/cn/edu/hitsz/compiler/lexer/LexicalAnalyzer.java
+++ src/cn/edu/hitsz/compiler/lexer/LexicalAnalyzer.java
@@ -1,9 +1,15 @@
 package cn.edu.hitsz.compiler.lexer;
 
 import cn.edu.hitsz.compiler.NotImplementedException;
+import cn.edu.hitsz.compiler.error.ErrorDescription;
 import cn.edu.hitsz.compiler.symtab.SymbolTable;
 import cn.edu.hitsz.compiler.utils.FileUtils;
 
+import java.io.*;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 /**
@@ -11,11 +17,14 @@ import java.util.stream.StreamSupport;
  * <br>
  * 你可能需要参考的框架代码如下:
  *
+ * @author chiro
  * @see Token 词法单元的实现
  * @see TokenKind 词法单元类型的实现
  */
 public class LexicalAnalyzer {
     private final SymbolTable symbolTable;
+    private String codeAll = "";
+    private List<Token> tokens = new LinkedList<>();
 
     public LexicalAnalyzer(SymbolTable symbolTable) {
         this.symbolTable = symbolTable;
@@ -31,7 +40,22 @@ public class LexicalAnalyzer {
         // TODO: 词法分析前的缓冲区实现
         // 可自由实现各类缓冲区
         // 或直接采用完整读入方法
-        throw new NotImplementedException();
+        final var file = new File(path);
+        final var builder = new StringBuilder();
+        try {
+            final var fileInputStream = new FileInputStream(file);
+            final var inputStreamReader = new InputStreamReader(fileInputStream);
+            final var reader = new BufferedReader(inputStreamReader);
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+                // 保持行号
+                builder.append("\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        codeAll = builder.toString();
     }
 
     /**
@@ -40,6 +64,93 @@ public class LexicalAnalyzer {
      */
     public void run() {
         // TODO: 自动机实现的词法分析过程
+        int state = 0;
+        final var accepts = new HashSet<>(Arrays.asList(15, 17, 19, 20, 22, 23, 25, 26, 27, 28, 29, 30, 31, 32));
+        final var keyWords = new HashSet<>(Arrays.asList(
+                "int",
+                "return"
+        ));
+        final var iterator = new StringCharacterIterator(codeAll);
+        StringBuilder idCode = new StringBuilder();
+        StringBuilder number = new StringBuilder();
+        while (iterator.current() != CharacterIterator.DONE) {
+            while (!accepts.contains(state) && iterator.current() != CharacterIterator.DONE) {
+                final var c = iterator.current();
+                boolean blank = c == ' ' || c == '\t' || c == '\n';
+                boolean digital = '0' <= c && c <= '9';
+                boolean letter = ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_';
+                int nextState = switch (state) {
+                    case 0 -> switch (c) {
+                        case '*' -> 18;
+                        case '=' -> 21;
+                        case '\"' -> 24;
+                        case '(' -> 26;
+                        case ')' -> 27;
+                        case ';' -> 28;
+                        case '+' -> 29;
+                        case '-' -> 30;
+                        case '/' -> 31;
+                        case ',' -> 32;
+                        default -> {
+                            if (blank) {
+                                yield 0;
+                            }
+                            if (digital) {
+                                yield 16;
+                            }
+                            if (letter) {
+                                yield 14;
+                            }
+                            yield -1;
+                        }
+                    };
+                    case 14 -> (letter || digital) ? 14 : 15;
+                    case 16 -> digital ? 16 : 17;
+                    case 18 -> c == '*' ? 19 : 20;
+                    case 21 -> c == '=' ? 22 : 23;
+                    case 24 -> (letter || digital) ? 24 : c == '\"' ? 25 : -1;
+                    default -> accepts.contains(state) ? 0 : -1;
+                };
+                switch (state) {
+                    case 0:
+                        if (letter) {
+                            idCode.append(c);
+                        }
+                        if (digital) {
+                            number.append(c);
+                        }
+                        break;
+                    case 14:
+                        if (nextState != 14 && nextState != 15) {
+                            idCode.setLength(0);
+                        }
+                    default:
+                        break;
+                }
+                state = nextState;
+                iterator.next();
+            }
+            if (accepts.contains(state)) {
+                tokens.add(switch (state) {
+                    case 15 -> {
+                        final var string = idCode.toString();
+                        idCode.setLength(0);
+                        yield keyWords.contains(string) ? Token.simple(string) : Token.normal("id", string);
+                    }
+                    case 17 -> {
+                        final var token = Token.normal("intConst", number.toString());
+                        number.setLength(0);
+                        yield token;
+                    }
+                    case 19 -> Token.simple("*");
+                    case 20 -> Token.simple("NULTI");
+                    case 22 -> Token.simple("=");
+                    case 23 -> Token.simple("ASSIGN");
+                    default -> throw new RuntimeException(String.format(ErrorDescription.LEX_NO_STATE, state));
+                });
+            }
+
+        }
         throw new NotImplementedException();
     }
 
@@ -58,8 +169,8 @@ public class LexicalAnalyzer {
 
     public void dumpTokens(String path) {
         FileUtils.writeLines(
-            path,
-            StreamSupport.stream(getTokens().spliterator(), false).map(Token::toString).toList()
+                path,
+                StreamSupport.stream(getTokens().spliterator(), false).map(Token::toString).toList()
         );
     }
 
